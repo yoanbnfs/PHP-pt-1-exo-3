152d18a12a8ea2515b9d9b67729146bd39020f85
Yoan Bonnefis <yoan.bonnefis@yahoo.fr>
2017-12-12 14:11:53 +0100

    exercice

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>exo-3</title>
+  </head>
+  <body>
+    <?php
+      $km = 1 . ' ';
+      echo $km;
+      $km = 3 . ' ';
+      echo $km;
+      $km = 125;
+      echo $km;
+    ?>
+  </body>
+</html>
